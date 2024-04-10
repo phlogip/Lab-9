ae0a608e8bd68661c668d4e3f4dd41a6b3e2ce40
phlogip <166570487+phlogip@users.noreply.github.com>
2024-04-09 21:36:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello")
+def encoder(password):
+  list = []
+  for number in password:
+    if int(number)+3 >= 7:
+      list.append(int(number)-7)
+    else:
+      list.append(int(number)+3)
+  return list
